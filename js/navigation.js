b07ca42481fb8ac3ca50c3c780133cd41a84ee8f
Felix Klotzsche <felixklotzsche@googlemail.com>
2013-12-01 21:02:37 +0100

    added to logic to remove style attribute from navigation on resize

--- js/navigation.js
+++ js/navigation.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
 	$("#nav-toggle").click(function(){
 		$("#navigation").slideToggle();
 	});
+
+
+	// just to be sure...
+	// remove the style attribute that was added
+	// through the code above on resize
+	$(window).resize(function(){
+		if(window.screen.width > 767){
+			$("#navigation").removeAttr("style");
+		}
+	})
 });
